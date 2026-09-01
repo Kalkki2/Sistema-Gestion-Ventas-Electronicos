7e002e1d8e5419644de52bec07fa4ebac896be3c
Mauricio <maurilencinas07@gmail.com>
2026-09-01 09:47:28 -0300

    .

--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaPresentacion.Cajero;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMenuAdministrador());
+            Application.Run(new FormMenuCajero());
         }
     }
 }
